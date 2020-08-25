d41fc7a0feaa626416dd3c2bae88542b65643ebd
Chester Alan <chesteralantagudin@gmail.com>
2020-08-25 17:56:56 +0800

    Added Netlify Admin

---
+++ content/index.js
@@ -0,0 +1,52 @@
+// index.js
+const fs = require('fs')
+const parseMarkdown = require('front-matter-markdown')
+
+/**
+ * getFiles - Get list of files in directory
+ * @param {string} dir
+ * @returns {Array} Array of objects
+ */
+const getFiles = (dir) => {
+  const files = fs.readdirSync(dir)
+  const filelist = []
+
+  files.forEach((file) => {
+    const markdownFile = fs.readFileSync(`${dir}${file}`, 'utf-8')
+    const fileContents = parseMarkdown(markdownFile)
+    const date = fileContents.date
+    const slug = file
+      .split('.')
+      .slice(0, -1)
+      .join('.')
+
+    const obj = { date, slug }
+
+    filelist.push(obj)
+  })
+  return filelist
+}
+
+/**
+ * Write blogs json file
+ */
+const writeJsonRoutes = async (dir, fname) => {
+  const fileArray = await getFiles(dir)
+
+  // Order array by date (default asc)
+  const sortedArray = await fileArray.sort((a, b) => {
+    return a.date.getTime() - b.date.getTime()
+  })
+
+  // Reverse array and write to JSON
+  const reversedArray = await sortedArray.reverse()
+  const jsonContent = await JSON.stringify(reversedArray)
+
+  fs.writeFile(`content/${fname}.json`, jsonContent, (err) => {
+    if (err) { throw new Error(err) }
+  })
+}
+
+writeJsonRoutes('content/blog/', 'blogs')
+writeJsonRoutes('content/services/', 'services')
+writeJsonRoutes('content/clients/', 'clients')
